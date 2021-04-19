7d55d857967cf1f8d5d7918d1e99368e7147e61c
Yue Zhang <zhangyue19890116@gmail.com>
2021-04-19 00:21:08 -0700

    "day 3"

--- app3.py
+++ app3.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,Response
 import settings
 
 app=Flask(__name__)
@@ -7,6 +7,10 @@ app.config.from_object(settings)
 @app.route("/")
 def show():
     return "<font color='red'> Hellow Yuezhang </font>"
+#返回的字符串其实也是封装了一个Response的class
+@app.route("/index4")
+def return_Response():
+    return Response("<h1>大家想吃什么？</h1>")
 
 if __name__ == "__main__":
     app.run(port=8080)
