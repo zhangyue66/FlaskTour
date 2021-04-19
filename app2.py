04b7d8722e95ed48287c31edcaa963587cbb679b
Yue Zhang <zhangyue19890116@gmail.com>
2021-04-19 00:09:24 -0700

    "day 3"

--- app2.py
+++ app2.py
@@ -8,8 +8,13 @@ app.config.from_object(settings)
 def projects():
     return 'The project page'
 # http://127.0.0.1:5000/projects/ 和 http://127.0.0.1:5000/projects都不会报错
+
+# 127.0.0.1 - - [19/Apr/2021 00:02:41] "GET /projects HTTP/1.1" 308 -
+
+
 @app.route('/about')
 def about():
     return 'The about page'
 #http://127.0.0.1:5000/about/ 会报错
+# 127.0.0.1 - - [19/Apr/2021 00:02:57] "GET /about/ HTTP/1.1" 404 - Not Found
 app.run()
