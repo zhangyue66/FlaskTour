6967c6fa6557d57aa0be504d5fef86b3d7103e1c
Yue Zhang <zhangyue19890116@gmail.com>
2021-04-21 01:09:49 -0700

    0421

--- app3.py
+++ app3.py
@@ -10,7 +10,17 @@ def show():
 #返回的字符串其实也是封装了一个Response的class
 @app.route("/index3")
 def return_Response():
+
     return Response("<h1>大家想吃什么？</h1>")
+
+@app.route("/yz")
+def return_res():
+    response = Response("hello")
+    print(response.headers)
+    print(response.status_code)
+    print(response.content_type)
+    return response
+
 
 @app.route("/index4")
 def index4():
---
+++ app4.py
@@ -0,0 +1,31 @@
+from flask import Flask, make_response, request
+import settings
+
+app = Flask(__name__)
+app.config.from_object(settings)
+
+@app.route("/index")
+def hello_word():
+    #make_response()
+    print(request.headers)
+    """
+    Host: 127.0.0.1:5000
+    Connection: keep-alive
+    Cache-Control: max-age=0
+    Sec-Ch-Ua: " Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"
+    Sec-Ch-Ua-Mobile: ?0
+    Upgrade-Insecure-Requests: 1
+    User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36
+    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+    Sec-Fetch-Site: none
+    Sec-Fetch-Mode: navigate
+    Sec-Fetch-User: ?1
+    Sec-Fetch-Dest: document
+    Accept-Encoding: gzip, deflate, br
+    Accept-Language: zh-CN,zh;q=0.9,en;q=0.8
+    """
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
